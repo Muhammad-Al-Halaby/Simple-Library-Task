699c6149c0cf043dd262b1010592bda2a47cc1f1
OsamaMaani <thephilologueprog@gmail.com>
2021-05-04 08:04:50 +0200

    fix port number

--- src/library/data/DBConnection.java
+++ src/library/data/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DBConnection {
     private static final  String HOST = "127.0.0.1";
-    private static final  int PORT = 3306;
+    private static final  int PORT = 80;
     private static final String DB_NAME = "Library_DB";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "";
