86c067f7a13c92a31b624b63cea6c5cea7b1a6d6
Muhammad Al-Halaby <moj1323@gmail.com>
2021-05-04 07:52:42 +0200

    Add pojo classes

--- src/library/pojo/Publisher.java
+++ src/library/pojo/Publisher.java
@@ -1,5 +1,27 @@
 package library.pojo;
 
 public class Publisher {
-    
+    private int publisher_id;
+    private String publisher_name;
+
+    public Publisher(int publisher_id, String publisher_name) {
+        this.publisher_id = publisher_id;
+        this.publisher_name = publisher_name;
+    }
+
+    public int getPublisher_id() {
+        return publisher_id;
+    }
+
+    public String getPublisher_name() {
+        return publisher_name;
+    }
+
+    public void setPublisher_id(int publisher_id) {
+        this.publisher_id = publisher_id;
+    }
+
+    public void setPublisher_name(String publisher_name) {
+        this.publisher_name = publisher_name;
+    }
 }
